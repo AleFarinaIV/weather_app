d3a5e04f71679252934c3e1776a4d0e05bbb557d
AleFarinaIV <Alessandrofarina401@gmail.com>
2025-03-04 22:08:28 +0100

    [UPD] used Nominatim API to get data about city/country from lat and long values -- need to improve the accuracy of geolocation

--- src/App.vue
+++ src/App.vue
@@ -38,45 +38,70 @@ export default {
             store.latitude = position.coords.latitude;
             store.longitude = position.coords.longitude;
             store.loading = false;
+
+            this.getCity();
             this.getWeatherData();
           },
           (error) => {
             store.loading = false;
             this.errorMessage = `Errore nella geolocalizzazione: ${error.message}`;
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0,
           }
         );
       } else {
         this.errorMessage = "Geolocation is not supported by your browser.";
       }
     },
+    getCity() {
+      if (store.latitude && store.longitude) {
+        // Nominatim API
+        let apiCity = `reverse?lat=${store.latitude}&lon=${store.longitude}&zoom=10&addressdetails=1`
+        let cityData = `${store.apiCityUrl}${apiCity}&format=json`;
+  
+        axios.get(cityData).then((result) => {
+          if (result.data && result.data.address) {
+            const address = result.data.address;
+            const city = address.city || address.town || address.village || address.county;
+            const country = result.data.address.country;
+
+            console.log(`Città: ${city}, Paese: ${country}`);
+            store.city = city;
+            store.country = country;
+          } else {
+            console.warn("Dati non trovati nella risposta di reverse geocoding:", response.data);
+          }
+        }).catch(error => {
+          console.error("Errore nella richiesta di reverse geocoding:", error);
+        })
+      } else {
+        console.warn("Latitudine e longitudine non disponibili.");
+      }
+    },
     getWeatherData() {
-      console.log("getWeatherData chiamata!"); // Per vedere se la funzione parte
-      let apiLocation = `forecast?latitude=${store.latitude}&longitude=${store.longitude}&timezone=auto`;
-      let weatherValue = `${store.apiUrl}${apiLocation}${store.currentApi}`;
+      if (store.latitude && store.longitude) {
+        console.log("getWeatherData chiamata!"); // Per vedere se la funzione parte
+        const apiLocation = `forecast?latitude=${store.latitude}&longitude=${store.longitude}&timezone=auto`;
+        const weatherValue = `${store.apiUrl}${apiLocation}${store.currentApi}`;
+  
+        axios.get(weatherValue).then((result) => {
+          console.log("Risultato API:", result); // Per vedere cosa riceviamo
+          if (result.data) {
+            store.valueArray = result.data.current;
+            store.unitsArray = result.data.current_units;
+            console.log("Dati salvati in store.valueArray:", store.valueArray);
+            console.log("Dati salvati in store.unitsArray:", store.unitsArray);
+          } else {
+            console.warn("Dati non trovati in result.data:", result.data);
+          }
+        }).catch((error) => {
+          console.error("Errore nella richiesta API: ", error);
+        });
 
-      axios.get(weatherValue).then((result) => {
-        console.log("Risultato API:", result); // Per vedere cosa riceviamo
-        if (result.data) {
-          store.valueArray = result.data.current;
-          console.log("Dati salvati in store.valueArray:", store.valueArray);
-        } else {
-          console.warn("Dati non trovati in result.data:", result.data);
-        }
-      }).catch((error) => {
-        console.error("Errore nella richiesta API: ", error);
-      });
-
-      axios.get(weatherValue).then((result) => {
-        console.log("Risultato API:", result); // Per vedere cosa riceviamo
-        if (result.data) {
-          store.unitsArray = result.data.current_units;
-          console.log("Dati salvati in store.unitsArray:", store.unitsArray);
-        } else {
-          console.warn("Dati non trovati in result.data:", result.data);
-        }
-      }).catch((error) => {
-        console.error("Errore nella richiesta API: ", error);
-      })
+      }
     }
   },
   data() {
